f76cfd5665ee05b21850751d177d54d314baf263
generous-vector-9581 <ojappah@gmail.com>
2019-08-27 19:53:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,12 +17,13 @@ array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-array = ["wow", "I", "am", "really", "learning"]
+array = ["wow", "I", "am", "really", "learning","arrays!"]
 array.pop
 end
 
 def remove_element_from_start_of_array(array)
-my_array.shift  
+array = ["wow", "I", "am", "really", "learning","arrays!"]
+array.shift  
 end
 
 def retrieve_element_from_index(array, index_number)
